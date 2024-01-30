a13a4c10dcd52904dbb358d384737bb3f607fe11
Suraj Patil <113511921+suraj1849@users.noreply.github.com>
2024-01-31 02:16:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-
-//   Modal open function
-const openModal = () => {
-  console.log("Modal is Open");
-  modal.classList.add("active");
-  overlay.classList.add("overlayactive");
-};
 
 // Modal close function
 const closeModal = () => {
