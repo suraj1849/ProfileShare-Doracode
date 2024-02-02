2267360608028342466e7db0429b3baeb7aa2201
Suraj Patil <113511921+suraj1849@users.noreply.github.com>
2024-02-03 00:29:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ const openModal = () => {
   overlay.classList.add("overlayactive");
 };
 
+// Modal close function
+const closeModal = () => {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+};
 
